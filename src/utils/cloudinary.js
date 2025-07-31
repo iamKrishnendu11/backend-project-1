f1d8392e82498769b2cbb3a975588425b209f331
iamKrishnendu11 <mandalkrishnendu1119@gmail.com>
2025-07-31 12:04:30 +0530

    learn the usage of multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+// reusable
+
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+
+
+        // upload an image
+
+        const uploadonCloudinary = async (localFilePath) => {
+            try {
+                if(!localFilePath) return null;
+               const response = await cloudinary.uploader.upload(localFilePath, {
+
+                })
+                //file has been uploaded successfully
+                console.log(`File uploaded successfully to Cloudinary: ${localFilePath}`, response.url);
+                    return response
+            } catch (error) {
+                fs.unlinkSync(localFilePath); // delete the file if upload fails
+                console.error(`Error uploading file to Cloudinary: ${error.message}`);
+                return null;
+            }
+        }
+
+        export { uploadonCloudinary }
