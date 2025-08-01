239fee70aee5cc9a7169b349b5476416181650c6
iamKrishnendu11 <mandalkrishnendu1119@gmail.com>
2025-08-01 22:15:57 +0530

    add getUserChannelProfile function to fetch user channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -379,6 +379,70 @@ const updatecoverImage = asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200, user, "cover image updated sucessfully"))
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+
+    const {username} = req.params
+
+    if(!username?.trim()) {
+        throw new ApiError(400, "username is required")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            },
+
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscriptions"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscriberCount: {$size: "$subscribers"},
+                subscribedToCount: {$size: "$subscribedTo"},
+                isSubscribed: {
+                 $cond: {
+                    if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                    then: true,
+                    else: false
+                }
+            }
+        }
+    },
+   { $project: {
+        fullname: 1,
+        username: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscriberCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1
+    }}
+
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404,"channel does not exist")
+    }
+    return res.status(200).json(
+        new ApiResponse(200, channel[0],"User channel fetched sucessfully")
+    )
+})
+
 export {
     registerUser, 
     loginUser,
