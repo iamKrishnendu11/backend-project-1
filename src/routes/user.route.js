a470c9ef4d4c1d6dfe94f2726da518edf68763a9
iamKrishnendu11 <mandalkrishnendu1119@gmail.com>
2025-08-02 00:04:12 +0530

    refactor user routes: organize imports and add missing routes for account management and user profile

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,20 @@
 // repeatated code
 import { Router } from "express";
-import { loginUser, logoutUser, refershAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+     changeCurrentPassword,
+     getCurrentUser,
+     getUserChannelProfile,
+     getWatchHistory,
+     loginUser,
+     logoutUser,
+     refershAccessToken,
+     registerUser,
+     updateAccoutDetails,
+     updateAvatar,
+     updatecoverImage
+} from "../controllers/user.controller.js";
+
+
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -29,5 +43,13 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refershAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword) 
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccoutDetails )  // varify jwt means only access to logged user
+router.route("/avatar").patch(verifyJWT,upload.single("/avatar"),updateAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage",updatecoverImage))
+router.route("/watch-histroy").get(verifyJWT,getWatchHistory)
+// when we using params
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
 export default router
