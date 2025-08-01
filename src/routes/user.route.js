45464b53d168d5a2376670844dfc34e8b61723d2
iamKrishnendu11 <mandalkrishnendu1119@gmail.com>
2025-08-02 00:21:05 +0530

    typo error

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -46,8 +46,8 @@ router.route("/refresh-token").post(refershAccessToken)
 router.route("/change-password").post(verifyJWT,changeCurrentPassword) 
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccoutDetails )  // varify jwt means only access to logged user
-router.route("/avatar").patch(verifyJWT,upload.single("/avatar"),updateAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage",updatecoverImage))
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updatecoverImage)
 router.route("/watch-histroy").get(verifyJWT,getWatchHistory)
 // when we using params
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
